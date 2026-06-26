d93994d82f9876ea29564f9d3edec0e3b44f5faf
WilliamSalcedo <wsl_1994@hotmail.com>
2026-06-25 20:23:35 -0500

    feat: add endpoint to create user in database after cognito signup

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -4,9 +4,11 @@ const {
   getProfile,
   updateProfile,
   deleteProfile,
+  createUser,
 } = require("../controllers/userController");
 const { verifyToken } = require("../middleware/auth");
 
+router.post("/", createUser);
 router.get("/perfil", verifyToken, getProfile);
 router.put("/perfil", verifyToken, updateProfile);
 router.delete("/perfil", verifyToken, deleteProfile);
